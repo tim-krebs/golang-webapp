fe47f0b8c778e0b4bf3f8ea4cc570c3fe8af1d50
tim-krebs <timkrebs9@gmail.com>
2023-02-02 14:07:43 +0100

    defined user structs

--- backend/UserService/data/user.go
+++ backend/UserService/data/user.go
@@ -1,1 +1,58 @@
 package data
+
+import (
+	"time"
+)
+
+type User struct {
+	ID         string    `bson:"_id"`
+	Password   []byte    `bson:"password"`
+	First_Name string    `bson:"first_name"`
+	Last_Name  string    `bson:"last_name"`
+	Birthday   time.Time `bson:"birthday"`
+	Avatar     string    `bson:"avatar"`
+	Email      string    `bson:"email"`
+	TelNr      string    `bson:"telNr"`
+	Company    string    `bson:"company"`
+	Occupation string    `bson:"occupation"`
+	School     string    `bson:"school"`
+	Subject    string    `bson:"subject"`
+	Country    string    `bson:"country"`
+	IsActive   bool      `bson:"isActive"`
+	Bio        string    `bson:"bio"`
+	Role       []int     `bson:"role"`
+}
+
+type UserRequest struct {
+	ID         string    `json:"id"`
+	Password   string    `json:"password"`
+	First_Name string    `json:"first_name"`
+	Last_Name  string    `json:"last_name"`
+	Birthday   time.Time `json:"birthday"`
+	Avatar     string    `json:"avatar"`
+	Email      string    `json:"email"`
+	TelNr      string    `json:"telNr"`
+	Company    string    `json:"company"`
+	Occupation string    `json:"occupation"`
+	School     string    `json:"school"`
+	Subject    string    `json:"subject"`
+	Country    string    `json:"country"`
+	IsActive   bool      `bson:"isActive"`
+	Bio        string    `bson:"bio"`
+	Role       []int     `bson:"role"`
+}
+
+type UpdateUserRequest struct {
+	Password   string    `json:"password"`
+	First_Name string    `json:"first_name"`
+	Last_Name  string    `json:"last_name"`
+	Birthday   time.Time `json:"birthday"`
+	Avatar     string    `json:"avatar"`
+	Email      string    `json:"email"`
+	TelNr      string    `json:"telNr"`
+	Company    string    `json:"company"`
+	Occupation string    `json:"occupation"`
+	School     string    `json:"school"`
+	Subject    string    `json:"subject"`
+	Country    string    `json:"country"`
+}
